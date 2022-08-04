378662b9488a44a8abd3beda40945463e69dc2f6
swxtz <alf4r6@gmail.com>
2022-08-03 21:30:05 -0300

    theme

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-const navigation = document.getElementById('navigation')
 
+
+const navigation = document.getElementById('navigation');
+const backButton = document.getElementById('backToTopButton');
 
 function onScroll() {
+    showNavOnScroll()
+    backToTop()
+}
+
+function backToTop() {
+    if(scrollY > 700) {
+        backButton.classList.add('show')
+    } else {
+        backButton.classList.remove('show')
+
+    }
+}
+
+function showNavOnScroll() {
     if(scrollY > 0) {
         navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
+
     }
 }
 
@@ -19,17 +36,29 @@ function closeMenu() {
 
 
 
+
 ScrollReveal({
     origin: 'top',
     distance: '30px',
     duration: 700,
 
 }).reveal(`#home,
- #home img,
+#home img,
 #home .stats,
 #services,
 #services header,
 #services .card,
 #about,
 #about header,
-#about .content`);
+#about .content, 
+#contact,
+#contact header,
+#contact ul,
+#contact li`);
+
+ScrollReveal({origin: 'left', distance: '30px', duration:700}).reveal(`
+footer .logo,
+footer p,
+footer .social-links
+`
+);
